b8aa0ffb6f9a7a162a75778033c7e70446f0f820
Marcel Schwittlick <info@schwittlick.net>
2021-12-11 16:56:30 +0100

    adds storage file in originate helper script

--- tripoly/originate.sh
+++ tripoly/originate.sh
@@ -6,5 +6,5 @@
 # ligo dry-run main.mligo main 'Dice(6n)' '(Map.literal[0n, {current_stock=1n; token_address=("KT1CE144SvpAv4iUQb8Zey4M3SKQQEV9DWMd" : address); token_price=1tz}], Map.literal [("tz1MEiHXRpHFmptzJyx4taqCmTHAYbcLpZUi" : address), {name="Marcel"; position=0n; saved_co2_kilos=0n; last_interaction=("2020-10-11t13:55:10Z" : timestamp)};])' --sender=tz1MEiHXRpHFmptzJyx4taqCmTHAYbcLpZUi --balance=10 --now='2021-01-01T10:10:10Z'
 
 ligo compile-contract main.mligo main --output-file=main.tz
-ligo compile-storage main.mligo main '(Map.literal[0n, {current_stock=1n; token_address=("KT1CE144SvpAv4iUQb8Zey4M3SKQQEV9DWMd" : address); token_price=1tz}], Map.literal [("tz1MEiHXRpHFmptzJyx4taqCmTHAYbcLpZUi" : address), {name="Marcel"; position=0n; saved_co2_kilos=0n; last_interaction=("2020-10-11t13:55:10Z" : timestamp)};])' --output-file=main_storage.tz
+ligo compile-storage main.mligo main "`cat main_storage.mligo`" --output-file=main_storage.tz
 tezos-client -E https://rpc.hangzhounet.teztnets.xyz originate contract main transferring 5 from schwittlick-testing running main.tz --init "`cat main_storage.tz`" --burn-cap 2 --force
